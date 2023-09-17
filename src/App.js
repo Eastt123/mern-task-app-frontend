4830e60c952f3458b62e8b5581de8fdfd50f139e
Eastt123 <58124751+Eastt123@users.noreply.github.com>
2023-09-18 03:39:15 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import TaskList from "./components/TaskList";
 import "./index.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+export const URL = process.env.REACT_APP_SERVER_URL;
 function App() {
   return (
     <div className="app">
